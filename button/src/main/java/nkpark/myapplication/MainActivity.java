ab64c75d3c2f313dbfa6eb8677b7441d39335ed9
exam1 <nkpark1017@naver.com>
2015-10-01 19:56:56 +0900

    Image Change

--- button/src/main/java/nkpark/myapplication/MainActivity.java
+++ button/src/main/java/nkpark/myapplication/MainActivity.java
@@ -1,18 +1,65 @@
 package nkpark.myapplication;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Gallery;
+
+import nkpark.myapplication.R;
 
 public class MainActivity extends Activity {
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        button0 = (Button)findViewById(R.id.button0);
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
 
+        button0.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.nate.com")));
+            }
+        });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:911")));
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media")));
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
--- calc/src/main/java/nkpark/myapplication/MainActivity.java
+++ calc/src/main/java/nkpark/myapplication/MainActivity.java
@@ -4,34 +4,118 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import nkpark.myapplication.R;
+
 
 public class MainActivity extends Activity {
+    EditText edit1, edit2;
+    Button btnAdd, btnSub, btnMul, btnDiv, btn;
+    TextView textResult;
+    String num1, num2;
+    Double result;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        setTitle("초간단 계산기");
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        edit1 = (EditText) findViewById(R.id.Edit1);
+        edit2 = (EditText) findViewById(R.id.Edit2);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        btnAdd = (Button) findViewById(R.id.BtnAdd);
+        btnSub = (Button) findViewById(R.id.BtnSub);
+        btnMul = (Button) findViewById(R.id.BtnMul);
+        btnDiv = (Button) findViewById(R.id.BtnDiv);
+        btn = (Button) findViewById(R.id.Btn);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        textResult = (TextView) findViewById(R.id.TextResult);
 
-        return super.onOptionsItemSelected(item);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                if (num1.isEmpty())
+                    Toast.makeText(getApplicationContext(), "숫자 입력", Toast.LENGTH_SHORT).show();
+                else if (num2.isEmpty())
+                    Toast.makeText(getApplicationContext(), "숫자 입력", Toast.LENGTH_SHORT).show();
+                else {
+                    result = Double.parseDouble(num1) + Double.parseDouble(num2);
+                    textResult.setText("계산 결과 : " + result.toString());
+                }
+
+            }
+        });
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                if (num1.isEmpty())
+                    Toast.makeText(getApplicationContext(), "숫자 입력", Toast.LENGTH_SHORT).show();
+                else if (num2.isEmpty())
+                    Toast.makeText(getApplicationContext(), "숫자 입력", Toast.LENGTH_SHORT).show();
+                else {
+                    result = Double.parseDouble(num1) - Double.parseDouble(num2);
+                    textResult.setText("계산 결과 : " + result.toString());
+                }
+            }
+        });
+        btnMul.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                if (num1.isEmpty())
+                    Toast.makeText(getApplicationContext(), "숫자 입력", Toast.LENGTH_SHORT).show();
+                else if (num2.isEmpty())
+                    Toast.makeText(getApplicationContext(), "숫자 입력", Toast.LENGTH_SHORT).show();
+                else {
+                    result = Double.parseDouble(num1) * Double.parseDouble(num2);
+                    textResult.setText("계산 결과 : " + result.toString());
+                }
+            }
+        });
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                if (num1.isEmpty())
+                    Toast.makeText(getApplicationContext(), "숫자 입력", Toast.LENGTH_SHORT).show();
+                else if (num2.isEmpty())
+                    Toast.makeText(getApplicationContext(), "숫자 입력", Toast.LENGTH_SHORT).show();
+                else if( Double.parseDouble(num2) == 0)
+                    Toast.makeText(getApplicationContext(), "0으로 나누기 불가능", Toast.LENGTH_SHORT).show();
+                else {
+                    result = Double.parseDouble(num1) / Double.parseDouble(num2);
+                    textResult.setText("계산 결과 : " + result.toString());
+                }
+            }
+        });
+        btn.setOnClickListener(new View.OnClickListener()
+
+        {
+
+            public void onClick(View view) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                if (num1.isEmpty())
+                    Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_SHORT).show();
+                else if (num2.isEmpty())
+                    Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_SHORT).show();
+                else if( Double.parseDouble(num2) == 0)
+                    Toast.makeText(getApplicationContext(), "0으로 나누기 불가능", Toast.LENGTH_SHORT).show();
+                else {
+                    result = Double.parseDouble(num1) % Double.parseDouble(num2);
+                    textResult.setText("계산 결과 : " + result.toString());
+                }
+
+            }
+        });
     }
 }
